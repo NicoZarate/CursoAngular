cc2f2c643771aa262d7638ce46bfee9cfe6ec110
NicoZarate <horacio.zarate@despegar.com>
2019-01-21 13:37:12 -0300

    Basic Service

--- conFusion/src/app/app.module.ts
+++ conFusion/src/app/app.module.ts
@@ -14,6 +14,7 @@ import { MatButtonModule } from '@angular/material/button';
 import "hammerjs";
 import { MenuComponent } from './menu/menu.component';
 import { DishdetailComponent } from './dishdetail/dishdetail.component' ;
+import { DishService } from './services/dish.service';
 
 @NgModule({
   declarations: [
@@ -31,7 +32,7 @@ import { DishdetailComponent } from './dishdetail/dishdetail.component' ;
     MatListModule,
     FlexLayoutModule
   ],
-  providers: [],
+  providers: [DishService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
